193209c9fc7079b3d8b9a56f8a582acb1ccd37e4
petecoj <petecojohns@gmail.com>
2018-07-06 11:03:06 -0500

    initial commit front end

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,7 @@
 console.log( 'js' );
+const app = angular.module('KoalaApp',[]);
+app.controller('KoalaController',['$http', function($http){
+    console.log('controller works ');
+    let self = this;
+    
+}])
