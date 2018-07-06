e5088abbd7e7699f7bfaa525b62b151e98475524
petecoj <petecojohns@gmail.com>
2018-07-06 11:33:31 -0500

    set up front end get and post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,35 @@
-console.log( 'js' );
-const app = angular.module('KoalaApp',[]);
-app.controller('KoalaController',['$http', function($http){
+console.log('js');
+const app = angular.module('KoalaApp', []);
+app.controller('KoalaController', ['$http', function ($http) {
     console.log('controller works ');
     let self = this;
-    
+    self.koalas =[];
+    self.addKoala = function (newKoala) {
+        console.log('click working');
+        $http({
+            url: '/koala',
+            method: 'POST',
+            data: newKoala
+        }).then(function(response){
+            console.log('made it to POST', response);
+            self.getKoalas();
+        }).catch(function(error){
+            console.log('error in POST', error);
+        })
+    }
+    self.getKoalas = function(){
+        $http({
+            url: '/koala',
+            method: 'GET'
+        }).then(function(response){
+            console.log('made it to GET', response);
+            console.log(response.data);
+            
+            self.koalas = response.data
+        }).catch(function(error){
+            console.log('error on GET', error);
+        })
+    }
+
+    self.getKoalas()
 }])
