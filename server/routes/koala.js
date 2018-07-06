dd3fe93b5a83109581115fa450ec9180ea6b578e
Tyler Sehr <tysehr37@gmail.com>
2018-07-06 11:27:02 -0500

    added get and post

--- server/routes/koala.js
+++ server/routes/koala.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Koala = require('../models/koala.schema');
 //require schemas here
 router.get('/', (req, res)=>{
-
+    console.log('got to pets GET');
+    Koala.find({}).then((data)=>{
+        console.log('here is our data', data);
+        res.send(data);
+    }).catch( (error)=>{
+        console.log('error on GET', error);
+        res.sendStatus(500);
+    });
 });
+
+router.post('/', (req, res)=>{
+    console.log('got to koalas post');
+    let newKoala = new Koala(req.body);
+
+    newPet.save().then( (data)=>{
+        console.log(data);
+        res.sendStatus(201);
+    }).catch( (error)=> {
+        console.log(error);
+        res.sendStatus(500);
+    })
+})
+
+module.exports = router;
