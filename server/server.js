fc5cb61bf3680881faff430798f476bbec346b6d
Tyler Sehr <tysehr37@gmail.com>
2018-07-06 11:31:18 -0500

    added router functions in serverjs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const koalaRouter = require('./routes/koala');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
 
 const mongoose = require('mongoose');
 const databaseUrl = 'mongodb://localhost:27017/koalaholla'
@@ -19,22 +21,22 @@ mongoose.connection.on('error', (error)=>{
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // This line is required for Angular
+app.use('/koala', koalaRouter);
+// // Routes
+// // Should these be in a router?
 
-// Routes
-// Should these be in a router?
+// // POST
+// app.post('/koala', (req, res) => {
+//   console.log('POST to /koala req.body =', req.body);
+// });
 
-// POST
-app.post('/koala', (req, res) => {
-  console.log('POST to /koala req.body =', req.body);
-});
-
-// GET
-app.get('/koala', (req, res) => {
-  // Temporary mock data. Replace this with mongoose.
-  const mockData = [{ _id: 1, name: 'Mock 1', gender: 'F', age: 5, ready_to_transfer: true, notes: 'n/a' },
-                    { _id: 2, name: 'Mock 2', gender: 'M', age: 5, ready_to_transfer: false, notes: 'n/a' }]
-  res.send(mockData);
-})
+// // GET
+// app.get('/koala', (req, res) => {
+//   // Temporary mock data. Replace this with mongoose.
+//   const mockData = [{ _id: 1, name: 'Mock 1', gender: 'F', age: 5, ready_to_transfer: true, notes: 'n/a' },
+//                     { _id: 2, name: 'Mock 2', gender: 'M', age: 5, ready_to_transfer: false, notes: 'n/a' }]
+//   res.send(mockData);
+// })
 
 // Static files
 app.use(express.static('server/public'));
